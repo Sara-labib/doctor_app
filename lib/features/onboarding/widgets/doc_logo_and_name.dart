993303ea0374686a831d0ed3a1f117055d4a700c
Sara-labib <saralabib048@gmail.com>
2025-07-10 15:49:02 +0300

    resize

--- lib/features/onboarding/widgets/doc_logo_and_name.dart
+++ lib/features/onboarding/widgets/doc_logo_and_name.dart
@@ -12,7 +12,7 @@ class DocLogoAndName extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SvgPicture.asset('assets/svgs/docdoc_logo.svg',),
-        SizedBox(width: 10.w),
+        SizedBox(width: 5.w),
         Text(
           'Docdoc',
           style: TextStyles.font248Black700Weight,
